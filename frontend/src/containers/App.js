6e3441a82abe045be910dfdd700dd36498789abc
powotato <30554162+1OwO1@users.noreply.github.com>
2024-06-08 19:33:57 +0800

    not yet display kb, but finish design of home and register, and fix the hook

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -20,7 +20,6 @@ function App() {
   return (
     <div className="App">
       <SearchBar keyIn={keyIn} setKeyIn={setKeyIn} />
-      <DisplayKB />
       <Router>
         <Switch>
           <HomeRoute exact path="/">
