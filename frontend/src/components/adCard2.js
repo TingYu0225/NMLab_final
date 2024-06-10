fb92705888bc7504697a30ffd093ea9997959fc9
tingwu <tingwu92220205@gmail.com>
2024-06-10 22:40:36 +0800

    add new ad cards, localization

---
+++ frontend/src/components/adCard2.js
@@ -0,0 +1,30 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { TextField } from "@mui/material";
+
+const AdCard2 = ({ title, description, actionText }) => {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardContent>
+        <Typography gutterBottom variant="h5" align="left" style={{ fontWeight: 'bold' }}>
+          {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary" align="left">
+          {description}
+        </Typography>
+      </CardContent>
+      <CardActions sx={{ justifyContent: "flex-end" }}>
+        <Button size="large" variant="contained" style={{  width: "100%" }}>
+          {actionText}
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
+
+export default AdCard2;
